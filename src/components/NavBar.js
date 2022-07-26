15a2cfb22ff70dd74215dd00a62dbaab6e852b0f
gbonfrisco <gustavobonfrisco@gmail.com>
2022-07-26 19:05:59 -0300

    revision json

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import "./NavBar.css" 
+export default function NavBar(){
+    return(
+
+        <ul>
+            <a>Home</a>
+            <a>Productos</a>
+            <a>Clientes</a>
+            <a>Contacto</a>
+        </ul>
+
+    )
+
+}
