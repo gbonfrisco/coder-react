41a3bfd6f2ec7e6db8f57eb73a1026af71825b76
gbonfrisco <gustavobonfrisco@gmail.com>
2022-07-28 21:43:39 -0300

    terminado logo y fondo del body

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from "./logoHP.png";
+import NavBar from "../Navbar/NavBar";
+
+export default function Header() {
+  return (
+    <header>
+      <a href="#" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+      <NavBar />
+    </header>
+  );
+}
