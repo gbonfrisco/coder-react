82a7938b94ed477a5ad4067f9eb45668707eb7fc
gbonfrisco <gustavobonfrisco@gmail.com>
2022-08-11 18:22:44 -0300

    terminado el producto detallado

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,45 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import data from "../Data/Data.js";
 import ItemDetail from "./ItemDetail.js";
 
-
-
 function getProduct() {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(data[0]), 2000);
-    });
-  }
-
-
-
-
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(data[0]), 2000);
+  });
+}
 
 function ItemDetailContainer() {
-
-    const [item, setItem] = useState([]);
+  const [item, setItem] = useState([]);
 
   useEffect(() => {
     getProduct().then((respuesta) => setItem(respuesta));
   }, []);
 
-
-  console.log(item)
   return (
-    <div>
-    {item.map((dataElem) => {
-      return (
-        <ItemDetail
-          key={dataElem.id}
-          tittle={dataElem.name}
-          desc={dataElem.description}
-          price={dataElem.price}
-          img={dataElem.img}
-          stock={dataElem.stock}
-        />
-      );
-    })}
-  </div>
-  )
+    <ItemDetail
+      key={item.id}
+      tittle={item.name}
+      desc={item.description}
+      price={item.price}
+      img={item.img}
+      stock={item.stock}
+    />
+  );
 }
-
 export default ItemDetailContainer;
